02f7467ede466877bc2c85282e3406a44b9a8707
Adam-Harte <adamharte11@hotmail.co.uk>
2018-11-30 15:39:14 +0000

    folder structure implemented and main components created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import CommentsBox from './components/CommentsBox';
+import CommentsList from './components/CommentsList';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-      
+        <CommentsBox />
+        <CommentsList />
       </div>
     );
   }
