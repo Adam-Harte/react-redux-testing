b039e303fe13a273951fa09b729a4fa47883ce46
Adam-Harte <adamharte11@hotmail.co.uk>
2018-11-30 16:43:16 +0000

    CommentsList component event handlers implemented

--- src/components/CommentsBox.js
+++ src/components/CommentsBox.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 
 class CommentsBox extends Component {
+   state = {
+      comment: '',
+      comments: []
+   };
+
+   handleChange = event => {
+      this.setState({ comment: event.target.value })
+   }
+
+   handleSubmit = event => {
+      event.preventDefault();
+
+      // TODO - call an action creator
+      // and save the comment
+
+      this.setState({ comment: '' });
+   }
+
    render() {
       return (
-         <form>
+         <form onSubmit={this.handleSubmit}>
             <h4>Add a Comment</h4>
-            <textarea />
+            <textarea value={this.state.comment} onChange={this.handleChange} />
             <div>
                <button>Submit</button>
             </div>
